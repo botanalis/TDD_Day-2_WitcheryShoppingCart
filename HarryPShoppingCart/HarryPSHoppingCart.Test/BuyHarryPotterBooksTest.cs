1b6ce3b8d08b8ea5b8c2b6f5ee6f464fca14378d
Makiwilb <codeeblan@hotmail.com>
2017-01-16 22:58:38 +0800

    新增需求-購買第1集(1本)、第2集(1本)、第3集(1本)，10%折扣-總金額270元

--- HarryPShoppingCart/HarryPSHoppingCart.Test/BuyHarryPotterBooksTest.cs
+++ HarryPShoppingCart/HarryPSHoppingCart.Test/BuyHarryPotterBooksTest.cs
@@ -37,6 +37,23 @@ namespace HarryPSHoppingCart.Test
             //assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void 購買第1集_1本_第2集_1本_第3集_1本_10_off折扣_總金額270元()
+        {
+            //arrange
+            PhenixCompany target = new PhenixCompany();
+            int expected = 270;
+
+            //act
+            target.Buy(new Book { Title = "哈利波特", Story = 1, Count = 1, Amount = 100 });
+            target.Buy(new Book { Title = "哈利波特", Story = 2, Count = 1, Amount = 100 });
+            target.Buy(new Book { Title = "哈利波特", Story = 3, Count = 1, Amount = 100 });
+            int actual = target.Settle();
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 
     
